37da50048dee3a9e6f60658ebd44336b1e8c71a2
harshpandya1234 <harshpandya428@gmail.com>
2025-04-18 00:10:44 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-<div className="flex flex-col md:flex-row min-h-screen">
+<div className="flex min-h-screen">
           <Sidebar />
   <div className="flex-1 ml-64">
             <main className="min-h-screen">
